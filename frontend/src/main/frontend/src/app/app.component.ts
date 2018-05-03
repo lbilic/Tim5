f3b312b1a6919af834582f72b1a150f08cc1c996
Luka Bilic <phoenix-023@hotmail.com>
2018-05-03 19:29:12 +0200

    Popravljen frontend

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { JwtService } from './services/jwt.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [JwtService],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
